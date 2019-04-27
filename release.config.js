9258b692396465a99da705c90149db913347cd06
Alex Van Camp <vancamp.alex@gmail.com>
2019-04-26 20:12:04 -0500

    build: enable success comment (#42)
    I disabled this for the first release because I didn't want it to go add comments to every single issue and PR ever closed in the history of this repo.

--- release.config.js
+++ release.config.js
@@ -11,8 +11,7 @@ module.exports = {
 		["@semantic-release/github", {
 			"assets": [
 				{"path": "addons/sourcemod/scripting/mgemod.smx", "label": "Compiled plugin"}
-			],
-			"successComment": false
+			]
 		}]
 	]
 };
